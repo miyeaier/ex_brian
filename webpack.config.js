b43511d7d5eb21b697b6df08b8a40a6f1dea8011
miyesier_kaerman <miyesier@gmail.com>
2020-02-02 22:50:07 +0100

    debug in webpack.config file about syntaex error.

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,46 @@ const path = require("path");
 const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
-module.exports ={
+module.exports = {
   entry: "./src/index.js",
-   mode: "development",
-   module: {
-    rules:[
+  mode: "development",
+  module: {
+    rules: [
       {
         test: /\.(js|jsx)$/,
-     exclude: /(node_modules)/,
-      loader: "babel-loader",
-     options: { presets: ["@babel/env"]}
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
       },
-     {
-       test: /\.css$/,
-       use:["style-loader", "css-loader"]
-     }
-     ]
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
-  resolve: { ectensions: ["*",".js","jsx"]
-   },
-   output: {
-     path: path.resolve(_dirname,"dist/"),
-     publicPath: "/dist/",  
-     filename: "/bundle.js"
-   },
-   devServer: {
-     contentBase: path.join(_dirname,"/"),
-     port:3000,
-     publicPath: "http://localhost:3000/dist/",
-     watchContentBase: true,
-     historyApiFallback: true
-   },
-   optimization: {
-     minimizar:[
-       new UglifyJsPlugin({
-         uglifyOptions: {
-           mangle: {
-             keep_fnames: true
-           }
-         }
-
-       })
-     ]
-   },
-   plugins: [new webpack.HotModuleReplacementPlugin()]
-}
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    watchContentBase: true,
+    historyApiFallback: true
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          mangle: {
+            keep_fnames: true
+          }
+        }
+      })
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
