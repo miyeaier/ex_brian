704cb16017f82d93115a9971941def2342fdf7e0
miyesier_kaerman <miyesier@gmail.com>
2020-02-09 21:18:42 +0100

    create a new file in src name hello.jsx and create out  Hello component d referencein fram index.js and  remane index.js to index.jsx.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const HelloWorld = () => {
-  return <h2> HelloWorld</h2>;
-};
-ReactDOM.render(<HelloWorld />, document.getElementById("app"));
